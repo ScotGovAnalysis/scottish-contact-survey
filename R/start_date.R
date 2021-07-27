a06934d01720a3184a48263d9edac5774a1a1fc3
alicebyers5 <aj3byers@gmail.com>
2021-07-27 17:38:07 +0100

    Change cohort to panel

--- R/start_date.R
+++ R/start_date.R
@@ -1,26 +1,31 @@
-#' @title Calculate first active day of CoMix survey.
+#' @title Date of the first day CoMix survey was open
 #'
-#' @param wave The number of survey wave in numeric format.
-#' @param cohort The survey cohort; either A or B.
+#' @description \code{start_date()} takes the wave number and panel of
+#' a CoMix survey and returns the date of the first day the survey was open.
 #'
-#' @return Returns a date corresponding to the day the survey opened.
-#' @export
+#' @param wave A numeric value denoting the survey wave number.
+#' @param panel A character value denoting the survey panel. Valid
+#' options are 'A' and 'B'.
+#'
+#' @return The date of the first day the survey was open.
 #'
 #' @examples
 #' # First date of Comix survey 24A
 #' start_date(24, "A")
+#'
+#' @export
 
-start_date <- function(wave, cohort = c("A", "B")){
+start_date <- function(wave, panel = c("A", "B")){
 
-  if(class(wave) != "numeric"){
+  if(!inherits(wave, "numeric")){
     stop("The wave number must be in numeric format.")
   }
 
-  cohort <- match.arg(cohort)
+  panel <- match.arg(panel)
 
   week_1_start_date <- dplyr::case_when(
-    cohort == "A" ~ lubridate::dmy(06082020),
-    cohort == "B" ~ lubridate::dmy(13082020)
+    panel == "A" ~ lubridate::dmy(06082020),
+    panel == "B" ~ lubridate::dmy(13082020)
   )
 
   week_1_start_date + lubridate::weeks((2 * wave) - 2)
