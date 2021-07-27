158d1385539dc5480caf37082a22763e41ebdb30
alicebyers5 <aj3byers@gmail.com>
2021-07-27 17:37:01 +0100

    Add error and warning

--- R/age_group.R
+++ R/age_group.R
@@ -1,19 +1,30 @@
 #' @title Create age groups
 #'
-#' @description \code{age_group()} takes a numeric vector and assignes each
+#' @description \code{age_group()} takes a numeric vector and assigns each
 #' age to the appropriate age group.
 #'
-#' @param age a vector of numeric values
+#' @param age A vector of numeric values.
 #'
-#' @return A character vector, where each element in the age group
+#' @return A character vector, where each element is the age group
 #' for the corresponding element in \code{age}.
-#' @export
 #'
 #' @examples
 #' age <- c(27, 30, 54, 89)
 #' age_group(age)
+#'
+#' @export
 
 age_group <- function(age){
+
+  if(!inherits(age, "numeric")){
+    stop("Ages must be in numeric format.")
+  }
+
+  if(any(age < 18)){
+    warning(paste("Ages under 18 are coded as NA as",
+                  "CoMix survey participants must be 18 or over."))
+  }
+
   dplyr::case_when(
     age %in% 18:29 ~ "18-29",
     age %in% 30:39 ~ "30-39",
@@ -22,4 +33,5 @@ age_group <- function(age){
     age %in% 60:69 ~ "60-69",
     age >= 70 ~ "70+"
   )
+
 }
