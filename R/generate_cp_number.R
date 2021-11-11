4283bb573cda4d1e8d83713dbbdfd9aade128b2c
alicebyers5 <aj3byers@gmail.com>
2021-11-11 13:48:51 +0000

    Allow any panel letter

--- R/generate_cp_number.R
+++ R/generate_cp_number.R
@@ -16,10 +16,10 @@
 #'
 #' @export
 
-generate_cp_number <- function(active_cp, panel = c("A", "B", "C", "D"), n){
+generate_cp_number <- function(active_cp, panel, n){
 
   # Check format of active_cp
-  if(any(!stringr::str_detect(active_cp, "^CP[A|B|C|D]\\d{4}$"), na.rm = TRUE)){
+  if(any(!stringr::str_detect(active_cp, "^CP[A-Z]\\d{4}$"), na.rm = TRUE)){
     stop("At least one CP number in incorrect format.")
   }
 
@@ -28,7 +28,10 @@ generate_cp_number <- function(active_cp, panel = c("A", "B", "C", "D"), n){
     stop("n must be in numeric format.")
   }
 
-  panel <- match.arg(panel)
+  # Check panel is correct format
+  if(!stringr::str_detect(panel, "^[A-Z]$")){
+    stop("panel must be a single upper case character; e.g. 'A'.")
+  }
 
   panel_cp <-
     active_cp[stringr::str_sub(active_cp, 3, 3) == panel & !is.na(active_cp)]
