47f897d948ee73067e51cede90c4ed58799c5c98
alicebyers5 <aj3byers@gmail.com>
2021-11-11 15:27:58 +0000

    Add to_update flag to invites

--- code/03_prep-next-wave.R
+++ code/03_prep-next-wave.R
@@ -74,9 +74,13 @@ write_rds(
 
 invites <-
   reg %>%
-  filter(status == "active" & panel == cur_panel) %>%
+  filter(status == "active" & str_sub(panel, 1, 1) == cur_panel) %>%
+
+  # Temp - add flag for old reserves - ask them to update data
+  mutate(to_update = ifelse(nchar(panel) == 2 & is.na(last_updated), 1, 0)) %>%
+
   select(email, contains("_name"), employment_status, studying_location,
-         vaccine_n_doses)
+         vaccine_n_doses, panel, to_update)
 
 write_csv(
   invites,
