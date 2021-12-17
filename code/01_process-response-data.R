cf90a6e82f47585018f31b8c21b0781178f9cd5d
alicebyers5 <aj3byers@gmail.com>
2021-12-17 16:52:47 +0000

    Keep all variables in response data

--- code/01_process-response-data.R
+++ code/01_process-response-data.R
@@ -30,7 +30,6 @@ resp <-
 
   # Clean names
   set_names(read_rds(here("lookups", "response-data-names.rds"))$new_names) %>%
-  select(-starts_with("temp"), -panel, -to_update) %>%
 
   # Fix formatting
   mutate(date_completed = janitor::excel_numeric_to_date(date_completed)) %>%
