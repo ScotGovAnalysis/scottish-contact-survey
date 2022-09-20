066577519df4141d4c075df1a017f849d24f1fdf
alicebyers5 <alice.byers@gov.scot>
2022-09-20 15:27:21 +0100

    Combine tests into one file per R/ file

--- tests/testthat/test-age.R
+++ tests/testthat/test-age.R
@@ -1,3 +1,5 @@
+# age
+
 test_that("Error if no panel supplied for wave 43 or earlier", {
   expect_error(age(as.Date("1980-03-30"), 2))
   expect_error(age(as.Date("1980-03-30"), 43))
@@ -24,4 +26,27 @@ test_that("Provides correct answer", {
   expect_equal(age(as.Date("1980-03-30"), 42, "B"), 41)
   expect_equal(age(as.Date("1980-03-30"), 44), 42)
   expect_equal(age(as.Date("1980-03-30"), 44, grouped = TRUE), "40-49")
+})
+
+
+# age_group
+
+test_that("Error if age isn't correct format", {
+  expect_error(age_group("eighty"))
+  expect_error(age_group(-50))
+  expect_error(age_group(50.5))
+  expect_error(age_group(c(25, 49.4)))
+})
+
+test_that("Warning if age 18 or less", {
+  expect_warning(age_group(15))
+  expect_warning(age_group(c(15, 30)))
+})
+
+test_that("Returns correct answer", {
+  expect_equal(age_group(40), "40-49")
+  expect_equal(age_group(62), "60-69")
+  expect_equal(age_group(89), "70+")
+  expect_equal(suppressWarnings(age_group(15)), NA_character_)
+  expect_equal(age_group(c(45, 71)), c("40-49", "70+"))
 })
